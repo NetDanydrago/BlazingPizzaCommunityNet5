1d3e113efe6861d25e10f34c6a644a7eaa789a51
Daniel Rosas Mialma <dan11_daniel@hotmail.com>
2021-08-27 10:19:02 -0500

    Siguiente Agregar un mensaje de confirmacion al eliminar pizzas

---
+++ BlazingPizza.ComponentsLibrary/Map/Map.razor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BlazingPizza.ComponentsLibrary.Map
+{
+    public partial class Map
+    {
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        [Parameter]
+        public List<Marker> Markers { get; set; }
+
+        public string ElementId { get; set; } = $"map--{Guid.NewGuid()}";
+
+        protected async override Task OnAfterRenderAsync(bool FirstRender)
+        {
+            await JSRuntime.InvokeVoidAsync("deliveryMap.showOrUpdate", ElementId, Markers);
+        }
+    }
+}
